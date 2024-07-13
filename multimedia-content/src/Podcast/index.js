4f01147a8c3d54e3311de3fe1ea725366f10116c
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-07-13 22:54:15 +0300

    add podcast

---
+++ multimedia-content/src/Podcast/index.js
@@ -0,0 +1,74 @@
+import "./index.css"
+function Podcast(){
+    return(
+        <div>
+            <hr></hr>
+        <div className="main">
+            <div className="content">
+                <h1>NEW ERA PODCASTING</h1>
+                <p>This podcasts include episodes made my Conqurers group
+                    <br></br>
+                    in the class of 2024 We'lle you distribute these podcast to apple podcasts,
+                    <br></br>
+                    spotify and Google Podcasts.
+                </p>
+             
+                <button>New Episodes</button>
+                </div>
+                <div>
+                <img src="/media/bluepodcast.jpg" alt="Podcast" className="podcast"></img>
+                </div>
+                <hr></hr>
+        </div>
+           <div className="main-content">
+            <div className="avatar">
+            <div>
+            <img src="media/herspace.jpeg" className="space"></img>
+            </div>
+        <div>
+<h2>Market research vs
+    <br></br>
+    Design research
+</h2>
+<p>Fair or not, it takes a lot of 
+   work to overcome a  <br></br>first impression, whether that impression was  <br></br>downright bad, or just different from expectations.</p>
+   <button>LISTEN NOW</button>
+   </div>
+       
+        </div>
+        <div className="avatar">
+            <div>
+            <img src="media/herspace.jpeg" className="space"></img>
+            </div>
+        <div>
+<h2>Market research vs
+    <br></br>
+    Design research
+</h2>
+<p>Fair or not, it takes a lot of 
+   work to overcome a  <br></br>first impression, whether that impression was  <br></br>downright bad, or just different from expectations.</p>
+   <button>LISTEN NOW</button>
+   </div>
+       
+        </div>
+        <div className="avatar">
+            <div>
+            <img src="media/herspace.jpeg" className="space"></img>
+            </div>
+        <div>
+<h2>Market research vs
+    <br></br>
+    Design research
+</h2>
+<p>Fair or not, it takes a lot of 
+   work to overcome a  <br></br>first impression, whether that impression was  <br></br>downright bad, or just different from expectations.</p>
+   <button>LISTEN NOW</button>
+   </div>
+       
+        </div>
+           </div>
+           <hr></hr>
+           </div>
+    )
+}
+export default Podcast;
