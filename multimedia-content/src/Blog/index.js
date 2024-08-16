1be4b910910795949db2d4a5c4c47d68e0733e17
Raziah <abdullahiraziah@gmail.com>
2024-08-16 12:44:10 +0300

    worked on article

--- multimedia-content/src/Blog/index.js
+++ multimedia-content/src/Blog/index.js
@@ -2,8 +2,7 @@ import './index.css'
 
 function Blog(){
     return(
-        <div>
-<div>
+<div className='container-blog'>
     
        <div className='blog-text'>
         <p className='blog'>Femme Blogs</p>
@@ -11,28 +10,24 @@ function Blog(){
     <div className='blog-layout-container'>
 
      <div className='book-cover'>
-        <img src='images/Born A crime.jpg' className='big'></img>
+        <img src='images/Born A crime.jpg' className='image'></img>
         </div>
 
     <div className='aerial-view-book'>
-        <img src='images/aerial view book.jpg' className='big'></img>
+        <img src='images/aerial view book.jpg' className='image'></img>
     </div>
 
     <div className='books-and-flowers'>
-        <img src = 'images/books and flowers.jpg' className='big'></img>
+        <img src = 'images/books and flowers.jpg' className='image'></img>
 
     </div>
     
         </div>
-        <div className='button'>
-            <button>Read More</button>
-        </div>
-
+        
 
 </div>    
    
 
-        </div>
     )
 }
 
--- multimedia-content/src/Review/review.js
+++ multimedia-content/src/Review/review.js
@@ -1,10 +1,44 @@
-import './review.js'
+import './review.css'
 
 function Review(){
     return(
- <div>
-    
+        <div>
+    <div className = 'blog-review-container'>
+    <div className ='landing-page'>
+        <div>
+        <img src = './images/blog page1.png' className ='blogs'></img>
+    </div>
+
+    <div className ='political-issue'>
+    <img src ='./images/political issue.png' className ='blogs'></img>
+</div>
+</div>
+<div>
+    <div className = 'economic-issue'>
+        <img src= './images/economic issue.png' className ='blogs'></img>
+    </div>
+
+   <div className = 'social-issue'>
+    <img src = './images/social issue.png' className = 'blogs'>
+
+    </img>
+   </div>
+   
  </div>
+ 
+ <div className = 'article'>
+    <img src = './images/article.png' className = 'blogs'>
+
+    </img>
+    </div>
+ </div>
+</div>
+ 
+
+        
+ 
+
+
     )
 }
 
